f703ed0cd461696366b401f3f4d5ef6dca1b137f
Huy Ngo <ngodotronghuy@gmail.com>
2022-08-24 11:23:53 +0700

    a

--- db/init_db.sql
+++ db/init_db.sql
@@ -1,30 +1,28 @@
-CREATE TABLE covid.country
+CREATE TABLE IF NOT EXISTS country
 (
     country_id serial NOT NULL,
     country_name character(200) NOT NULL,
+    region_id bigint,
     confirmed bigint NOT NULL,
     recovered bigint NOT NULL,
     deaths bigint NOT NULL,
-    country_population bigint NOT NULL,
+    population bigint NOT NULL,
     sq_km_area bigint NOT NULL,
-    life_expectancy numeric NOT NULL,
-    elevation_in_meters numeric NOT NULL,
-    continent character(20) NOT NULL,
-    abbreviation character(10) NOT NULL,
+    life_expectancy character(20) NOT NULL,
+    elevation_in_meters character(200) NOT NULL,
+    continent character(100) NOT NULL,
+    abbreviation character(50) NOT NULL,
     location character(200) NOT NULL,
-    iso smallint NOT NULL,
+    iso character(50) NOT NULL,
     capital_city character(200) NOT NULL,
-    lat numeric NOT NULL,
-    "long" numeric NOT NULL,
+    lat character(200) NOT NULL,
+    "long" character(200) NOT NULL,
     created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
-    CONSTRAINT country_pk PRIMARY KEY (country_id)
+    updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    PRIMARY KEY (country_id)
 );
 
-
-);
-
-CREATE TABLE covid.region (
+CREATE TABLE IF NOT EXISTS region (
     region_id serial NOT NULL,
     country_id bigint NOT NULL,
     region_name character(200) NOT NULL,
@@ -34,11 +32,7 @@ CREATE TABLE covid.region (
     recovered bigint NOT NULL,
     deaths bigint NOT NULL,
     created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
-    CONSTRAINT region_pk PRIMARY KEY (region_id),
-    CONSTRAINT region_fk FOREIGN KEY (region_id)
-        REFERENCES public.country (country_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID
+    updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    PRIMARY KEY (region_id),
+    FOREIGN KEY (country_id) REFERENCES country(country_id) 
 );
